89b0a1188e150ae4eb1eb4b6dcbc8c8cbfd7862a
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-10-02 11:30:04 +0900

    アカウント編集機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if current_user.update(user_params)
+      redirect_to root_path
+   else
+      render :edit
+   end
+  end
+  
+  private
+  
+  def user_params
+     params.require(:user).permit(:nickname, :email, :password, :first_name, :family_name, :first_name_kana, :family_name_kana, :department_id, :birthday, :prefecture_id, :hobby, :free)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'documents/index'
   root to: "documents#index"
+  resources :users, only: [:edit, :update]
 end
